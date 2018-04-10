121a42bf7bca25c8c186223a41dae761d7009a8e
AlexisCesi <alexis.hoyez@viacesi.fr>
2018-04-10 09:44:00 +0200

    PHP/HTML

---
+++ Projet/Index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<!--####################################
+ Auteur : Groupe 3
+ Date : 2018
+ Contexte : Projet Web Exia CESI
+ #######################################-->
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/style.css" />
+
+        <title>BDE Arras</title>
+    </head>
+
+    <body>
+
+    <!-- L'en-tête -->    
+    <header>
+
+    </header>
+
+    
+
+    <!-- Le corps -->
+    <div id="corps">
+
+        <!-- Le menu -->
+         <?php include("menu.php"); ?>
+
+    </div>
+    
+    </body>
+
+</html>
