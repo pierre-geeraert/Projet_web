94b73ad541025738cae810a7d04bc2f7f95f030b
pierre-geeraert <pierre.geeraert@viacesi.fr>
2018-04-10 23:28:48 +0200

    :sparkles: :globe_with_meridians: change languages of MCD and add request

--- sql/connexion.sql
+++ sql/connexion.sql
@@ -1,16 +1,15 @@
 DROP  PROCEDURE IF EXISTS connexion;
+
 DELIMITER | -- Facultatif si votre délimiteur est toujours |
+
 CREATE PROCEDURE connexion(IN nom VARCHAR(200),IN Prenom VARCHAR(200))  
     
 BEGIN
     	select "login good" 
 	from utilisateurs 
-	where (
-		select 1 
-		from utilisateurs 
-		where Nom="brunelot" and Prenom="romain"
-		) limit 1;
+	where (select 1 from utilisateurs where Nom=nom and Prenom=Prenom) limit 1;
 END |
+
 DELIMITER ;  -- On remet le délimiteur par défaut
 
 
