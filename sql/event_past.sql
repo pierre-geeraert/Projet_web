74ad29c57871e65513c4563a60929827ae5f7887
pierre-geeraert <pierre.geeraert@viacesi.fr>
2018-04-12 17:05:44 +0200

    :zap: add other request sql

---
+++ sql/event_past.sql
@@ -0,0 +1,12 @@
+
+DROP  PROCEDURE IF EXISTS event_past;
+DELIMITER | -- Facultatif si votre délimiteur est toujours |
+CREATE PROCEDURE event_past()
+
+BEGIN
+  SELECT event, description FROM events WHERE DATEDIFF( event_date, NOW() ) < 0;
+
+
+END |
+DELIMITER ;  -- On remet le délimiteur par défaut
+
