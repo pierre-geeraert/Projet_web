ece155e5510b8f107cdc07d5879ce2e25acff9ff
AlexisCesi <alexis.hoyez@viacesi.fr>
2018-04-16 17:24:32 +0200

    Boite a idée avec validation

--- Projet/BoiteIdee.php
+++ Projet/BoiteIdee.php
@@ -32,6 +32,49 @@
 			<input type="submit" value="Envoyer" />
 		</form>
 		</div>
+		
+		<?php
+						
+			$bdd = new PDO('mysql:host=mysql-pi-ux-ce.alwaysdata.net;dbname=pi-ux-ce_web;charset=utf8', 'pi-ux-ce_web', 'cesi');
+							
+			$reponse = $bdd->query('SELECT event, description, event_id FROM events WHERE validation="0"');
+			$nbr_event=0;
+			while ($donnees = $reponse->fetch()){
+				$nbr_event++;
+				${'event'.$nbr_event}=$donnees['event'];
+				${'description'.$nbr_event}=$donnees['description'];
+				${'id'.$nbr_event}=$donnees['event_id'];
+			}
+							
+			for($var=1; $var <= $nbr_event; $var++){	
+				echo '
+				<ul class="center_boite">
+					<fieldset>
+					'.${'event'.$var}.'
+						<fieldset class="inner">
+															
+						<p>
+							'.${'description'.$var}.'
+						</p>
+						';
+						if (isset($_SESSION)) { 
+							if($_SESSION['statut'] === "BDE"){ 
+								echo '
+									<form id="'.$var.'" action="validate.php" method="post">
+										<input type="hidden" name="'.$var.'" value="'.${'id'.$var}.'"/>
+										<input type="hidden" name="nbr_event" value="'.$nbr_event.'"/>
+									</form> 
+									<a class="participate "href=\'#\' onclick=\'document.getElementById("'.$var.'").submit()\'> Valider l\'idée </a>
+								';
+							}
+						}
+						echo '								
+						</fieldset>
+					</fieldset>
+				</ul>
+				';
+			}					
+		?>
 
 
 	
---
+++ Projet/validate.php
@@ -0,0 +1,17 @@
+<?php   
+session_start();
+$bdd = new PDO('mysql:host=mysql-pi-ux-ce.alwaysdata.net;dbname=pi-ux-ce_web;charset=utf8', 'pi-ux-ce_web', 'cesi');
+
+$nbr = $_POST['nbr_event'];
+$user_id = $_SESSION['id'];
+$date="2018-08-18";
+
+for($var=1; $var<=$nbr ; $var++){
+	if (isset($_POST[$var])) {
+		$bdd->query('call validation_event('.$_POST[$var].','.$user_id.','.$date.' )');
+	}
+}
+
+header('Location: BoiteIdee.php');
+
+?>
