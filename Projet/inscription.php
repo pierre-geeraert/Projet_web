4d25991deab6e425d8c0f0b22d6691f59cca8172
JeanclementPdv <jeanclement.podevin@viacesi.fr>
2018-04-19 00:22:50 +0200

    Indent + comment

--- Projet/inscription.php
+++ Projet/inscription.php
@@ -29,30 +29,31 @@
     <section>
 			<div id="register" class="Inscri">
                 <form  method="post" action="scriptInscription.php" autocomplete="on"> 
+
+
                     <h1> Inscription </h1> 
 
                         <p> 
                                     <label for="lastnamesignup" class="uname"  > </label>
-                                    <input id="Nom" class="champ" name="Nom" required="required" type="text" placeholder="Nom" />
+                                    <input id="Nom" class="champ" name="Nom" required="required" type="text" placeholder="Nom" />  <!-- Enter a name -->
 						</p>
                       
                          <p> 
                                     <label for="firstnamesignup" class="uname"  > </label>
-                                    <input id="Prenom" class="champ" name="Prenom" required="required" type="text" placeholder="Prenom" />
+                                    <input id="Prenom" class="champ" name="Prenom" required="required" type="text" placeholder="Prenom" /> <!-- Enter a firstname -->
 						</p>
 						
 						<p> 
                                     <label for="usernamesignup" class="uname"  > </label>
-                                    <input id="Email" class="champ" name="Email" required="required" type="email" placeholder="Email" />
+                                    <input id="Email" class="champ" name="Email" required="required" type="email" placeholder="Email" /> <!-- Enter a mail -->
 						</p>
                         
 						<p> 
                                     <label for="passwordsignup" class="youpasswd"  > </label>
-                                
-                                    <input id="Mdp" class="champ" name="Mdp" required="required" type="password"  placeholder="Mdp" >
+                                    <input id="Mdp" class="champ" name="Mdp" required="required" type="password"  placeholder="Mdp" > <!-- Enter a password with some conditions : -->
                         </p>
                                      <div id="message">
-                                    <h3>Le mot de passe doit contenir:</h3>
+                                    <h3>Le mot de passe doit contenir:</h3>                  
                                     <p id="letter" class="invalid">Une <b>minuscule</b></p>
                                     <p id="capital" class="invalid">Une <b>majuscule</b> </p>
                                     <p id="number" class="invalid">Un<b>nombre</b></p>
--- Projet/panier.class.php
+++ Projet/panier.class.php
@@ -4,56 +4,56 @@ class panier{
 	private $DB;
 
 	public function __construct($DB){
-		if(!isset($_SESSION)){
+		if(!isset($_SESSION)){				// If Session does not exist , it is create
 			session_start();
 		}
 		if(!isset($_SESSION['panier'])){
-			$_SESSION['panier'] = array();
+			$_SESSION['panier'] = array();   // The Session will initiate with the lass 'panier' and have an array
 		}
 		$this->DB = $DB;
 
 		if(isset($_GET['delPanier'])){
-			$this->del($_GET['delPanier']);
+			$this->del($_GET['delPanier']);			//for del an elements of the session 'panier'
 		}
 		if(isset($_POST['panier']['quantity'])){
-			$this->recalc();
+			$this->recalc();						// quantity of different product
 		}
 	}
 
-	public function recalc(){
+	public function recalc(){										// function for recalc the total price of an order 
 		foreach($_SESSION['panier'] as $product_id => $quantity){
-			if(isset($_POST['panier']['quantity'][$product_id])){
+			if(isset($_POST['panier']['quantity'][$product_id])){			
 				$_SESSION['panier'][$product_id] = $_POST['panier']['quantity'][$product_id];
 			}
 		}
 	}
 
 	public function count(){
-		return array_sum($_SESSION['panier']);
+		return array_sum($_SESSION['panier']);		// count the sum of all products  price
 	}
 
 		public function total(){
 		$total = 0;
-		$ids = array_keys($_SESSION['panier']);
+		$ids = array_keys($_SESSION['panier']);		// function tatal of the panier 
 		if(empty($ids)){
 			$products = array();
 		}else{
-			$products = $this->DB->query('SELECT product_id, price FROM products WHERE product_id IN ('.implode(',',$ids).')');
+			$products = $this->DB->query('SELECT product_id, price FROM products WHERE product_id IN ('.implode(',',$ids).')');  // request in order to have the product id and his price 
 		}
 		foreach( $products as $product ) {
-			$total += $product->price * $_SESSION['panier'][$product->product_id];
+			$total += $product->price * $_SESSION['panier'][$product->product_id]; // the total = the price * the number of products
 		}
 		return $total;
 	}
 	public function add($product_id){
 		if(isset($_SESSION['panier'][$product_id])){
 			$_SESSION['panier'][$product_id]++;
-		}else{
+		}else{												// function to add a product in the 'panier'
 			$_SESSION['panier'][$product_id] = 1;
 		}
 	}
 
-	public function del($product_id){
+	public function del($product_id){					// function to DEL a product in the 'panier'
 		unset($_SESSION['panier'][$product_id]);
 	}
 
